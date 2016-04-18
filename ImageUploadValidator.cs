8ce96eebcbae7cb790f84bf772eea6c46bfd5dc4
Benton Landry <noodoggy@gmail.com>
2016-04-18 14:11:39 -0400

    Added files via upload

---
+++ ImageUploadValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Web;
+
+namespace CoderBBlog.Models.CodeFirst
+{
+    public static class ImageUploadValidator
+    {
+        public static bool IsWebFriendlyImage(HttpPostedFileBase file)
+        {
+            //check for actual objet
+            if (file == null)
+                return false;
+
+            //check size - file must be less than 2 MB and greater than 1 KB
+            if (file.ContentLength > 8 * 1024 * 1024 || file.ContentLength < 1024)
+                return false;
+
+            try
+            {
+                using (var img = Image.FromStream(file.InputStream))
+            
+                return ImageFormat.Jpeg.Equals(img.RawFormat) ||
+                           ImageFormat.Png.Equals(img.RawFormat) ||
+                           ImageFormat.Gif.Equals(img.RawFormat);
+            
+            }
+
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
